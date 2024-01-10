a2801aa249303443b63e02403685cb761637b762
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2024-01-11 03:51:07 +0530

    Added comments data context and comment list

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import styles from "./App.module.scss";
-import Comment from "./components/Comment/Comment";
 import CommentForm from "./components/CommentForm/CommentForm";
-
-const comment: Comment = {
-  id: 1,
-  name: "Prasad",
-  comment: "Test",
-  date: new Date(),
-  parentId: null,
-};
+import CommentList from "./components/CommentList.tsx/CommentList";
+import CommentsProvider from "./context/CommentsProvider";
 
 function App() {
   return (
     <div className={styles.wrapper}>
-      <CommentForm />
-      <Comment comment={comment} />
+      <CommentsProvider>
+        <CommentForm />
+        <CommentList />
+      </CommentsProvider>
     </div>
   );
 }
---
+++ src/context/CommentsContext.ts
@@ -0,0 +1,19 @@
+import { createContext, useContext } from "react";
+import { IComment } from "../types";
+
+interface CommentContextData {
+  comments: IComment[];
+  addComment: (comment: IComment) => void;
+  deleteComment: (commentId: number) => void;
+  updateComment: (commentId: number, updatedComment: IComment) => void;
+}
+
+const initialState: CommentContextData = {
+  comments: [],
+  addComment: () => {},
+  deleteComment: () => {},
+  updateComment: () => {},
+};
+
+export const CommentContext = createContext<CommentContextData>(initialState);
+export const useComments = () => useContext(CommentContext);
