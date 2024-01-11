7e0f2ba04e733cb6560ce9fe303f15caa014cf8f
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2024-01-11 20:58:54 +0530

    Added reply functionality

--- src/components/CommentList.tsx/CommentList.tsx
+++ src/components/CommentList.tsx/CommentList.tsx
@@ -1,14 +1,22 @@
-import { useComments } from "../../context/CommentsContext";
+import { IComment } from "../../types";
 import Comment from "../Comment/Comment";
 import styles from "./CommentList.module.scss";
 
-const CommentList = () => {
-  const { comments } = useComments();
+interface IProps {
+  comments: IComment[];
+}
+
+const CommentList = ({ comments }: IProps) => {
+  if (comments.length === 0) return <></>;
+
   return (
     <div className={styles.wrapper}>
       {comments.map((cm) => (
-        <div key={cm.id}>
+        <div className={styles.parent} key={cm.id}>
           <Comment comment={cm} />
+          <div className={styles.replies}>
+            <CommentList comments={cm.replies} />
+          </div>
         </div>
       ))}
     </div>
